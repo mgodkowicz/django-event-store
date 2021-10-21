93ba41024c464bd58ddf167978fbafd99ea8fe15
Michał Godkowicz <michal.godkowicz@tivix.com>
2021-10-21 18:37:05 +0200

    Enrich events metadata

--- event_store/client.py
+++ event_store/client.py
@@ -1,5 +1,6 @@
 import uuid
 from collections import Iterable
+from datetime import datetime
 from typing import Callable, List, Optional, Union
 
 from broker import Broker
@@ -22,12 +23,14 @@ class Client:
         subscriptions: Optional[Subscriptions] = None,
         dispatcher: Dispatcher = Dispatcher(),
         mapper=None,
+        clock: Callable = datetime.now,
     ):
         self.repository = repository
         self.subscriptions = subscriptions or Subscriptions()
         self.broker = Broker(self.subscriptions, dispatcher)
         self.correlation_id_generator = uuid.uuid4
         self.mapper = mapper
+        self.clock = clock
 
     def publish(
         self, events: Events, stream_name: str = GLOBAL_STREAM, expected_version=None
@@ -35,10 +38,13 @@ class Client:
         if not isinstance(events, Iterable):
             events = [events]
 
-        records = self._transform(events)
+        enriched_events = self._enrich_events_metadata(events)
+        records = self._transform(enriched_events)
         self.append_records_to_stream(list(records), stream_name, expected_version)
-        for event in events:
-            self.broker.call(event, None)
+
+        for event, record in zip(enriched_events, records):
+            self.broker.call(event, record)
+
         return self
 
     def append(
@@ -49,8 +55,13 @@ class Client:
     ) -> "Client":
         if not isinstance(events, Iterable):
             events = [events]
-        records = self._transform(events)
-        self.append_records_to_stream(records, stream_name, expected_version)
+
+        self.append_records_to_stream(
+            self._transform(self._enrich_events_metadata(events)),
+            stream_name,
+            expected_version,
+        )
+
         return self
 
     def subscribe(self, subscriber: Callable, to: List) -> "Client":
@@ -75,3 +86,14 @@ class Client:
 
     def _transform(self, events: Events) -> List[Record]:
         return [self.mapper.event_to_record(event) for event in events]
+
+    def _enrich_events_metadata(self, events: Events) -> Events:
+        for event in events:
+            self._enrich_event_metadata(event)
+        return events
+
+    def _enrich_event_metadata(self, event: Event) -> Event:
+        event.metadata["timestamp"] = self.clock()
+        event.metadata["valid_at"] = event.metadata["timestamp"]
+        event.metadata["correlation_id"] = self.correlation_id_generator()
+        return event
